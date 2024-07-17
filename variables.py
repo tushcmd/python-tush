ff7c24bbb29163980b4ab355148ff8983171d1c0
tushcmd <muturidavid854@gmail.com>
2024-07-17 04:56:16 +0300

    variables

--- variables.py
+++ variables.py
@@ -13,3 +13,42 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+# x = 1  # int
+
+# y = 2.5  # float
+
+# name = 'Tush'  # str
+
+# is_cool = True  # bool
+
+
+# # More from codieum
+# x = 1j  # complex
+# x = ["a", "b", "c"]  # list
+# x = ("a", "b", "c")  # tuple
+# x = range(6)  # range
+# x = {"a": 1, "b": 2}  # dict
+# x = {"a", "b", "c"}  # set
+# x = frozenset({"a", "b", "c"})  # frozenset
+
+# x = b"Hello"  # bytes
+# x = bytearray(5)  # bytearray
+# x = memoryview(bytes(5))  # memoryview
+
+# multiple assignment
+x, y, is_cool, name = (1, 2.5, True, 'Tush')
+
+# print(x, y, is_cool, name, a)
+
+
+# Basic math 
+
+a = x + y
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+print(type(z), z)

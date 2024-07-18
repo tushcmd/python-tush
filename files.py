68c53abe01e8727ddaa21edc5be0cfcbf3bf7416
tushcmd <muturidavid854@gmail.com>
2024-07-18 07:27:41 +0300

    working with files

--- files.py
+++ files.py
@@ -1,1 +1,30 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file for reading and writing
+# with open('test.txt', mode='r+') as file:
+#     content = file.read()
+#     print(content)
+
+myFile = open('test.txt', 'w')
+
+# Get some info
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write('\nI love JavaScript')
+myFile.close()
+
+
+# Append to file
+myFile = open('test.txt', 'a')
+myFile.write('\nI also like C')
+myFile.close()
+
+# Read from file
+myFile = open('test.txt', 'r+')
+text = myFile.read(100)
+print(text)
+myFile.close()
